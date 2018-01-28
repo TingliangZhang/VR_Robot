d52bf6e20760da9d02b00407e7a4c977d2f3053b
TingliangZhang <32335780+TingliangZhang@users.noreply.github.com>
2018-01-28 16:17:41 +0800

    py hhhhhhh

---
+++ Python/Practice/ex1.py
@@ -0,0 +1,8 @@
+# -*- coding: utf-8 -*-
+
+print"Hello World"
+print"hello again"
+print"I like typing this."
+print"Yay!Printing!"
+print'IOError'
+print u"我是张庭梁"
---
+++ Python/Practice/leapyear1.py
@@ -0,0 +1,5 @@
+y=int(input("请输入年份："))
+if (y % 400 == 0): print("是闰年")
+elif (y % 4 != 0): print("不是闰年")
+elif (y % 100 == 0): print("不是闰年")
+else: print("是闰年") 
