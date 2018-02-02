2e130144ff532e2dc2f5c53621abfcb5f060ce85
TingliangZhang <32335780+TingliangZhang@users.noreply.github.com>
2018-02-02 19:29:57 +0800

    hangman

--- Python/Practice/HangMan/HangMan/HangMan.py
+++ Python/Practice/HangMan/HangMan/HangMan.py
@@ -1,16 +1,22 @@
 import random
 words=['chichen','dog','cat','mouse','fog']
 lives_remaining = 14
+guessed_letters = ''
 
 def pick_a_word():
 	return(random.choice(words))
 
 def get_guess(word):
-	return 'a'
+	print_word_with_blanks(word)
+	print('Lives Remaining:'+ str(lives_remaining))
+	guess=input('Guess a letter or whole word?')
+	return guess
 
 def process_guess(guess,word):
 	global lives_remaining
+	global guessed_letters
 	lives_remaining = lives_remaining -1
+	guessed_letters = guessed_letters + guess
 	return False
 
 def play():
@@ -26,6 +32,16 @@ def play():
 			print('The word was:'+ word)
 			break
 		
+def print_word_with_blanks(word):
+	display_word=''
+	for letter in word:
+		if guessed_letters.find(letter) > -1:
+			#letter found
+			display_word=display_word+letter
+		else:
+			#letter_not_found
+			display_word= display_word + '-'
+	print (display_word)
 
 play()
 
