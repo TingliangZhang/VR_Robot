21af9a6f1a6258d5153163831477da4c44317bbe
TingliangZhang <32335780+TingliangZhang@users.noreply.github.com>
2018-02-28 21:42:59 +0800

    FIRSTBLOOD

--- 舵机/amarino1/amarino1.ino
+++ 舵机/amarino1/amarino1.ino
@@ -1,6 +1,14 @@
 #include <MeetAndroid.h>
-
+#include <Servo.h>
 MeetAndroid meetAndroid;
+
+float data[3] = {0};
+int intdata[3] = {0};
+int i = 1;
+Servo myservo1; //lower servo
+Servo myservo2; //upper servo
+int pos1 = 0;
+int pos2 = 0;
 
 void setup() {
   // put your setup code here, to run once:
@@ -12,7 +20,10 @@ void setup() {
   // - the first parameter is the name of your function (see below)
   // - match the second parameter ('A', 'B', 'a', etc...) with the flag on your Android application
   //   small letters are custom events, capital letters inbuilt Amarino events
-  meetAndroid.registerFunction(floatValues, 'A');
+  meetAndroid.registerFunction(floatValues, 'A'); //orientation
+  meetAndroid.registerFunction(intValue, 'B'); //Compass
+  myservo1.attach(10); //Attach the servo to pin eleven
+  myservo2.attach(11); //Attach the servo to pin eleven
 }
 
 void loop() {
@@ -27,6 +38,30 @@ void floatValue(byte flag, byte numOfValues)
 {
   float v = meetAndroid.getFloat();
   meetAndroid.send(v);
+}
+
+/**
+ * Be careful about int values.
+ * In Arduino int is a 16bit value!
+ * In Android int is a 32bit value!
+ *
+ * If you expect to receive int values which are
+ * not in this range (-32,768 to 32,767)
+ * you should use meetAndroid.getLong()
+ *
+ */
+void intValue(byte flag, byte numOfValues)
+{
+  int v = meetAndroid.getInt();
+  meetAndroid.send(v);
+  
+    if (0<=data[1]<=359)   
+  {
+  
+     pos1 = v/2;
+     myservo1.write(pos1);
+  }
+  
 }
 
 /*
@@ -49,4 +84,22 @@ void floatValues(byte flag, byte numOfValues)
   {
     meetAndroid.send(data[i]);
   }
+
+  if (-180<=data[2]<=180)   
+  {
+     intdata[1] = int(data[1]); //Cast the X axis float value to int
+     intdata[1] = intdata[1]+180;
+     pos2 = intdata[1]/2;
+     myservo2.write(pos2);
+  }
+
+//  if (-10<=data[3]<=10)   
+//  {
+//     
+//     data[3] = data[3]+10;
+//     pos2 = data[3]*9;
+//     myservo2.write(pos2);
+//  }
+  
+  
 }
